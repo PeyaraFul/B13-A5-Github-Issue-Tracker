715ba33970b59c090365a9e306e1495a9892ee56
PeyaraFul <arakash022@gmail.com>
2026-03-07 09:46:05 +0600

    home page layout is ok

---
+++ js/index.js
@@ -0,0 +1,16 @@
+
+// login part
+const userName = document.getElementById('username');
+const loginPassword = document.getElementById('password');
+
+function goHomePage(p){
+if(userName.value === 'admin' && loginPassword.value === 'admin123'){
+       
+        window.location.assign('./homePage.html') ;
+         alert('login successfully') ;
+    }
+    else{
+        alert('login unsuccessful')
+    }
+     
+}
