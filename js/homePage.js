9a653ad7d23d2de9d3a1dca4c469a560b8b89daa
PeyaraFul <arakash022@gmail.com>
2026-03-09 13:01:17 +0600

    search condition has been finished successflly

--- js/homePage.js
+++ js/homePage.js
@@ -1,77 +1,85 @@
+const allIssue = document.getElementById("all-issue");
+const openIssue = document.getElementById("open-issue");
+const closedIssue = document.getElementById("closed-issue");
 
-// {
-// "id": 1,
-// "title": "Fix navigation menu on mobile devices",
-// "description": "The navigation menu doesn't collapse properly on mobile devices. Need to fix the responsive behavior.",
-// "status": "open",
-// "labels": [
-// "bug",
-// "help wanted"
-// ],
-// "priority": "high",
-// "author": "john_doe",
-// "assignee": "jane_smith",
-// "createdAt": "2024-01-15T10:30:00Z",
-// "updatedAt": "2024-01-15T10:30:00Z"
-// },
-const allIssue = document.getElementById('all-issue') ;
-let elementOfNumberOfAllIssue = document.getElementById('number-of-all-issue')
- let numberOfAllIssue = elementOfNumberOfAllIssue.innerText ;
+// console.log(document.getElementsByClassName('tab'))
 
+let elementOfNumberOfAllIssue = document.getElementById("number-of-all-issue");
+let numberOfAllIssue = elementOfNumberOfAllIssue.innerText;
+
+const searchBtn = document.getElementById("search-btn");
+// console.log(searchBtn) ;
+
+let countAllIssue = 0;
+let countOpenIssue = 0;
+let countClosedIssue = 0;
+let searchData = '' ;
+let searchValue = '' ;
+
+
+searchBtn.addEventListener("click", function () {
+  searchValue = document.getElementById("search-value").value.trim().toLowerCase();
+
+  loadAllApi();
+  console.log(searchData);
+});
+console.log(searchValue);
+
+
+  
 
 
 async function loadAllApi() {
-    const response = await fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues');
-    const data = await response.json();
+  const response = await fetch(
+    "https://phi-lab-server.vercel.app/api/v1/lab/issues",
+  );
+  const data = await response.json();
 
-    // set number of all issue
-    let dataLength = data.data.length ;
-    elementOfNumberOfAllIssue.innerText = dataLength ;
+  const searchUrl =
+    "https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=";
 
+  const searchResponse = await fetch(searchUrl + searchValue);
+  searchData = await searchResponse.json();
+  // console.log(searchData) ;
 
-    function allIssueDataSet(){
-        allIssue.innerHTML= '' ;
-        data.data.forEach(element => {
+//   // set number of all issue
+//   let dataLength = data.data.length;
+//   elementOfNumberOfAllIssue.innerText = dataLength;
+  const issues = searchValue ? searchData.data  : data.data
 
-            
+  function divDataSet() {
+    allIssue.innerHTML = "";
+    openIssue.innerHTML = "";
+    closedIssue.innerHTML = ``;
+    
+    countAllIssue = 0 ;
+    countOpenIssue = 0 ;
+    countClosedIssue = 0 ;
+    issues.forEach((element) => {
+      
 
-            const dataTitle = element.title ;
-            const dataDescription = element.description ; 
-            const dataStatus = element.status ; 
-            const dataAuthor = element.author ; 
-            const dataPriority = element.priority ; 
-            const dataLabels = element.labels ; 
-            const dataLabels0 = element.labels[0] ; 
-            const dataCreatedAt = element.createdAt ;
-            const dataAssignee = element.assignee ; 
+      const dataTitle = element.title;
+      const dataDescription = element.description;
+      const dataStatus = element.status;
+      const dataAuthor = element.author;
+      const dataPriority = element.priority;
+      const dataLabels = element.labels;
+      const dataLabels0 = element.labels[0];
+      const dataCreatedAt = element.createdAt;
+      const dataAssignee = element.assignee;
 
-            function gettingDataLabels1(){
-                if(dataLabels.length ===2){
-                return element.labels[1] ;
+      function gettingDataLabels1() {
+        if (dataLabels.length === 2) {
+          return element.labels[1];
+        }
+      }
+      gettingDataLabels1();
+     
+      const div = document.createElement("div");
+      div.className = `card bg-base-100 w-96 shadow-sm p-4 m-auto border ${dataStatus === "open" ? "border-green-500" : "border-purple-500"} border-t-[6px]`;
+      div.setAttribute("onclick", `my_modal_1.showModal()`);
 
-                }
-            }
-            gettingDataLabels1() ;
-
-            // console.log(dataLabels2)
-            // console.log(dataLabels.length)
-
-
-
-            // Title
-            // Description
-            // Status
-            // Category
-            // Author
-            // Priority
-            // Label
-            // CreatedAt
-
-            const div = document.createElement('div') ;
-            div.className = `card bg-base-100 w-96 shadow-sm p-4 m-auto border ${dataStatus === 'open' ? 'border-green-500' : 'border-purple-500' } border-t-[6px]` ;
-            div.setAttribute('onclick', `my_modal_1.showModal()`);
-            
-            div.innerHTML= `            
+      div.innerHTML = `            
                 <div class="flex justify-between">
                     <img class="w-5" src="./assets/Open-Status.png" alt="">
                     <div class="badge badge-warning text-[#F59E0B] font-semibold bg-[#FFF6D1] "> ${dataPriority} </div>
@@ -81,60 +89,73 @@ async function loadAllApi() {
                     <p> ${dataDescription} </p>
                     <div class="card-actions flex justify-start gap-5">
                         <div class="badge badge-secondary font-semibold text-orange-500 bg-[#FECACA] "> ${dataLabels0.toUpperCase()} </div>
-                        <div class="badge badge-warning font-semibold text-yellow-700 bg-[#FDE68A] ${gettingDataLabels1() === undefined? 'hidden' : ''} "> ${gettingDataLabels1()} </div>
+                        <div class="badge badge-warning font-semibold text-yellow-700 bg-[#FDE68A] ${gettingDataLabels1() === undefined ? "hidden" : ""} "> ${gettingDataLabels1()} </div>
                     </div>
 
                     <div class="divider"></div>
                     <p> ${dataAuthor} </p>
                     <p> ${dataCreatedAt} </p>
-                </div>
+                </div>            
+            `;
+      const divCopy1 = div.cloneNode(true);
+      const divCopy2 = div.cloneNode(true);
 
-        
-            
-            `       
-
-            allIssue.appendChild(div) ;
-
-
-            div.addEventListener('click', function(){
-                const modalTitle = document.getElementById('modal-title');
-                const modalStatus = document.getElementById('modal-status');
-                const modalLabels0 = document.getElementById('modal-labels0');
-                const modalLabels1 = document.getElementById('modal-labels1');
-                const modalDescription = document.getElementById('modal-description');
-                const modalAssignee = document.getElementById('modal-assignee');
-                const modalPriority = document.getElementById('modal-priority');
-               
-
-                // console.log(modalTitle)
-                modalTitle.innerText = dataTitle ;
-                modalStatus.innerText = dataStatus;
-                modalLabels0.innerText = dataLabels0.toUpperCase() ;
-                modalLabels1.innerText = gettingDataLabels1().toUpperCase() ;
-                modalDescription.innerText = dataDescription ;
-                modalAssignee.innerText = dataAssignee ;
-                modalPriority.innerText = dataPriority ;
-
-            })
-
-        });
-        
+      
+      allIssue.appendChild(div);
+      countAllIssue = allIssue.children.length ;
+      if (dataStatus === "open") {
+        openIssue.appendChild(divCopy1);
+        countOpenIssue = openIssue.children.length;
+      }
+      if (dataStatus === "closed") {
+        closedIssue.appendChild(divCopy2);
+        countClosedIssue = closedIssue.children.length;
+      }
 
 
-    }
-    allIssueDataSet() ;
+      div.addEventListener("click", function () {
+        const modalAuthor = document.getElementById("modal-author");
+        const modalTitle = document.getElementById("modal-title");
+        const modalStatus = document.getElementById("modal-status");
+        const modalLabels0 = document.getElementById("modal-labels0");
+        const modalLabels1 = document.getElementById("modal-labels1");
+        const modalDescription = document.getElementById("modal-description");
+        const modalAssignee = document.getElementById("modal-assignee");
+        const modalPriority = document.getElementById("modal-priority");
+
+        // console.log(modalTitle)
+        modalAuthor.innerText = dataAuthor;
+        modalTitle.innerText = dataTitle;
+        modalStatus.innerText = dataStatus;
+        modalLabels0.innerText = dataLabels0.toUpperCase();
+        modalLabels1.innerText = gettingDataLabels1().toUpperCase();
+        modalDescription.innerText = dataDescription;
+        modalAssignee.innerText = dataAssignee;
+        modalPriority.innerText = dataPriority;
+      });
+    });
+
+    window.countIssue = function (id) {
+      if (id === "all-tab") {
+        elementOfNumberOfAllIssue.innerText = countAllIssue;
+      }
+
+      if (id === "open-tab") {
+        elementOfNumberOfAllIssue.innerText = countOpenIssue;
+      }
+      if (id === "closed-tab") {
+        elementOfNumberOfAllIssue.innerText = countClosedIssue;
+      }
+    };
+    window.countIssue('all-tab', 'open-tab', 'closed-tab') ;
+    
+    console.log(countClosedIssue) ;
+  }
+  divDataSet();
+}
+loadAllApi();
 
 
 
 
-
-
-  
-   }        
-   loadAllApi() ;
-
-
-
-
-           
 
